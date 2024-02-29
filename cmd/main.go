1f4433ff32ebbf490177fcdcbe400fb778eb7761
Andrey E <matteoup@ya.ru>
2024-02-29 22:47:43 +0300

    Сonfiguration using flags has been replaced by... configuration using environment variables. Added initDefaultDatabaseFile func. Edited .gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,13 @@ import (
 	"ToDoBot1/pkg/events/telegram"
 	"ToDoBot1/pkg/processorloop"
 	"ToDoBot1/pkg/storage/sqlite"
-	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	tgBotToken, dbPath := mustFlags()
+	tgBotToken, dbPath := loadCfgFromEnv()
 
 	// creating an object for interacting with telegram api
 	tgClient := tgC.New(tgBotToken)
@@ -31,19 +32,40 @@ func main() {
 }
 
 // mustFlags retrieves the value of flags (-tg-token, -db-path)
-func mustFlags() (string, string) {
-	tgToken := flag.String("tg-token", "", "telegram bot token")
-	dbPath := flag.String("db-path", "", "sqlite3 db file path")
-
-	flag.Parse()
-
-	if *tgToken == "" {
-		log.Fatal("missing telegram bot token (-tg-token flag)")
+func loadCfgFromEnv() (string, string) {
+	tgToken := os.Getenv("TG_TOKEN")
+	if tgToken == "" {
+		log.Fatal("missing telegram bot token 'TG_TOKEN' env var")
 	}
 
-	if *dbPath == "" {
-		log.Fatal("missing database file path (-db-path flag)")
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		log.Print("database path (DB_PATH env) not set, using default value = ./database/db1.sqlite3")
+
+		if err := initDefaultDatabaseFile(); err != nil {
+			log.Fatal(err.Error())
+		}
+
+		dbPath = "./database/db1.sqlite3"
 	}
 
-	return *tgToken, *dbPath
+	return tgToken, dbPath
+}
+
+func initDefaultDatabaseFile() error {
+	if _, err := os.Stat("./database"); os.IsNotExist(err) {
+		err = os.Mkdir("./database", 0755)
+		if err != nil {
+			return fmt.Errorf("can't create './database' folder: %w", err)
+		}
+	}
+
+	if _, err := os.Stat("./database/db1.sqlite3"); os.IsNotExist(err) {
+		file, err := os.Create("./database/db1.sqlite3")
+		if err != nil {
+			return fmt.Errorf("can't create database file './datavase/db1.sqlite3': %w", err)
+		}
+		file.Close()
+	}
+	return nil
 }
